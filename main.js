e7d3e3f1b67577f12df3f02ef75d557322d8ffb7
Songchangyeop <61750383+Songchangyeop@users.noreply.github.com>
2021-05-16 02:24:16 +0900

    전체적인 UI 변경

--- main.js
+++ main.js
@@ -4,32 +4,32 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 // Make navbar transparent when is on the top
-document.addEventListener('scroll', () => {
-  if (window.scrollY > navbarHeight) {
-    navbar.classList.add('navbar--dark');
-  } else {
-    navbar.classList.remove('navbar--dark');
-  }
-});
+// document.addEventListener('scroll', () => {
+//   if (window.scrollY > navbarHeight) {
+//     navbar.classList.add('navbar--dark');
+//   } else {
+//     navbar.classList.remove('navbar--dark');
+//   }
+// });
 
 // Handle scrolling when tapping in the navbar menu
 
-const navbarMenu = document.querySelector('.navbar__menu');
-navbarMenu.addEventListener('click', (event) => {
-  const target = event.target;
-  const link = target.dataset.link;
-  if (link == null) {
-    return;
-  }
-  navbarMenu.classList.remove('open');
-  scrollIntoView(link);
-});
+// const navbarMenu = document.querySelector('.navbar__menu');
+// navbarMenu.addEventListener('click', (event) => {
+//   const target = event.target;
+//   const link = target.dataset.link;
+//   if (link == null) {
+//     return;
+//   }
+//   navbarMenu.classList.remove('open');
+//   scrollIntoView(link);
+// });
 
 // Navbar toggle button for small screen
-const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
-navbarToggleBtn.addEventListener('click', () => {
-  navbarMenu.classList.toggle('open');
-});
+// const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+// navbarToggleBtn.addEventListener('click', () => {
+//   navbarMenu.classList.toggle('open');
+// });
 
 // Handle click on "contact me" button on home
 
